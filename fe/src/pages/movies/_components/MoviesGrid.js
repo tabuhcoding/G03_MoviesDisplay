9d625ebd18732681d91cfc593362f7cf0e4e3f55
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2024-12-10 15:30:31 +0700

    fix size title

--- fe/src/pages/movies/_components/MoviesGrid.js
+++ fe/src/pages/movies/_components/MoviesGrid.js
@@ -31,7 +31,11 @@ const MoviesGrid = ({ movies, lastMovieElementRef }) => {
                 alt={movie.title || 'Movie'}
               />
               <div className="movies-info">
-                <h3>{movie.title}</h3>
+                <h4 className={isExpanded ? "expanded-title" : "truncated-title"}>
+                    {isExpanded || movie.title.length <= 30
+                      ? movie.title
+                      : `${movie.title.substring(0, 30)}...`}
+                </h4>
                 <p>Rating: {movie.vote_average.toFixed(1)}</p>
                 <p>{movie.release_date ? new Date(movie.release_date).getFullYear() : 'Unknown'}</p>
                 {isExpanded && <p className="movie-overview">{movie.overview || 'No overview available.'}</p>}
