81338705369f995e32456c33a9c45f8fbac15f95
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2024-12-11 00:22:04 +0700

    abc

--- fe/src/components/ProfileRoute.js
+++ fe/src/components/ProfileRoute.js
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 import { useUser } from "../helpers/useContext";
 import { useFetchUserProfile } from "../helpers/useFetchProfile";
 
