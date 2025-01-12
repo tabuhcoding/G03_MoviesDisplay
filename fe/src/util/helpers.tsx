c90ea062e7b0300b8103e670d0d706ea62c165d9
DatTruong0108 <dattantruong.work@gmail.com>
2025-01-12 09:56:24 +0700

    refactor: separate helper method

--- fe/src/util/helpers.tsx
+++ fe/src/util/helpers.tsx
@@ -12,3 +12,27 @@ export const formatRunTimeToHHMM = (runtime: number) => {
   const minutes = runtime % 60;
   return `${hours}h ${minutes}m`;
 }
+
+interface FormatDateToDDMMYYYY {
+  (isoString: string): string;
+}
+
+export const formatIsoStringToDDMMYYYY: FormatDateToDDMMYYYY = (isoString) => {
+  const date = new Date(isoString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+  const year = date.getFullYear();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds} ${day}/${month}/${year}`;
+};
+
+export const formatDateToMonthYYYY = (dateString: string) => {
+  if (!dateString) return "";
+  const date = new Date(dateString);
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+    year: 'numeric'
+  }).format(date);
+};
