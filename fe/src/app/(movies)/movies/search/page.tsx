720d27fa85fd7a4188615c0a57324ffebd040b97
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2025-01-14 01:58:49 +0700

    Remove file

--- fe/src/app/(movies)/movies/search/page.tsx
+++ fe/src/app/(movies)/movies/search/page.tsx
@@ -7,7 +7,6 @@ import SearchInput from "@components/layout/searchInput";
 import MoviesGrid from "../../../(movies)/movies/[id]/_components/moviesGrid";
 import Pagination from "../../../(movies)/movies/[id]/_components/pagination";
 import { END_POINT_URL_LIST } from "@/src/util/constant";
-import FilterComponent from "./_components/filter";
 import '@public/styles/movie/search.css'
 import "@styles/Homepage.css";
 
@@ -30,13 +29,9 @@ export default function SearchPage() {
   const searchParams = useSearchParams();
   const query = searchParams.get("query") || ""; // Lấy giá trị từ URL
   const page = parseInt(searchParams.get("page") || "1");
-  // const [selectedFilter, setSelectedFilter] = useState<string>('Movies');
   const [active, setActive] = useState<string>("movies");
-  const [sortOption, setSortOption] = useState("Popularity Descending"); // Default sort
 
   useEffect(() => {
-    console.log("Current sortOption: ", sortOption); 
-
     const fetchMovies = async () => {
       if (!query) return;
       setIsLoading(true);
@@ -52,42 +47,7 @@ export default function SearchPage() {
         const results = response.data.data.results || [];
         console.log("Result old: ", results);
 
-        // Sắp xếp danh sách phim theo tùy chọn sortOption
-        const sortedMovies = [...results]; // Tạo bản sao của mảng để tránh thay đổi trực tiếp
-        switch (sortOption) {
-        case "Popularity Descending":
-          sortedMovies.sort((a, b) => (b.popularity || 0) - (a.popularity || 0));
-          break;
-        case "Popularity Ascending":
-          sortedMovies.sort((a, b) => (a.popularity || 0) - (b.popularity || 0));
-          break;
-        case "Rating Descending":
-          sortedMovies.sort((a, b) => (b.vote_average || 0) - (a.vote_average || 0));
-          break;
-        case "Rating Ascending":
-          sortedMovies.sort((a, b) => (a.vote_average || 0) - (b.vote_average || 0));
-          break;
-        case "Release Date Descending":
-          sortedMovies.sort(
-            (a, b) =>
-              new Date(b.release_date || "").getTime() - new Date(a.release_date || "").getTime()
-          );
-          break;
-        case "Release Date Ascending":
-          sortedMovies.sort(
-            (a, b) =>
-              new Date(a.release_date || "").getTime() - new Date(b.release_date || "").getTime()
-          );
-          break;    
-        case "Title A-Z":
-          sortedMovies.sort((a, b) => a.title.localeCompare(b.title)); // Sort by title A-Z
-          break;
-        case "Title Z-A":
-          sortedMovies.sort((a, b) => b.title.localeCompare(a.title)); // Sort by title Z-A
-          break;    
-        }
-      
-        setMovies(sortedMovies); 
+        setMovies(results);
         setTotalPages(response.data.data.total_pages || 1);
       } catch (error) {
         console.error("Error fetching movies:", error);
@@ -97,7 +57,7 @@ export default function SearchPage() {
     };
 
     fetchMovies();
-  }, [query, page, sortOption]);
+  }, [query, page]);
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -136,20 +96,15 @@ export default function SearchPage() {
               </div>
             </div>
 
-            <div className="row">
-              <div className="col-2">
-                <FilterComponent setSortOption={setSortOption} sortOption={sortOption}/>
-              </div>
-              <div className="col-10 mb-4">
-                <MoviesGrid movies={movies} />
-                <Pagination
-                  currentPage={page}
-                  totalPages={totalPages}
-                  onPageChange={(newPage) =>
-                    router.push(`/movies/search?query=${query}&page=${newPage}`)
-                  }
-                />
-              </div>
+            <div className="row mb-4">
+              <MoviesGrid movies={movies} />
+              <Pagination
+                currentPage={page}
+                totalPages={totalPages}
+                onPageChange={(newPage) =>
+                  router.push(`/movies/search?query=${query}&page=${newPage}`)
+                }
+              />
             </div>
           </>
         ) : (
