cbc7f5ce147914253f1be2c912307ab13f5a9a97
tabuhcoding <134000982+tabuhcoding@users.noreply.github.com>
2025-01-14 17:00:58 +0700

    fix type

--- fe/src/app/searchLLM/page.tsx
+++ fe/src/app/searchLLM/page.tsx
@@ -20,12 +20,13 @@ export interface Movie {
   id: number;
   title: string;
   overview: string;
-  poster_path: string;
-  backdrop_path: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
   release_date: string;
   vote_average: number;
   vote_count: number;
-  trailer_key?: string; 
+  trailer_key?: string;
+  popularity: number; 
 }
 
 export default function SearchLLM(){
